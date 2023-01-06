287a713b77a89c06b90f50bf51b3fc8cb88c27b5
Amit Roy <amitroygcettb15@gmail.com>
2023-01-06 12:53:31 +0530

    Initial commit

---
+++ container/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import MarketingApp from "./components/MarketingApp";
+
+const App = () => {
+  return (
+    <>
+      <h1>Hey there!!</h1>
+      <hr />
+      <MarketingApp />
+    </>
+  );
+};
+
+export default App;
